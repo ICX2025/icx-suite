f2d9de0f8387a71d23f0009e64b3b4a4f914ae37
ICX2025 <methode.ic.formation@gmail.com>
2025-07-12 16:46:52 +0200

    suppression lignes erronées

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur ICX-Suite Backend 🎧');
+  res.send('Bienvenue sur ICX-Suite Backend 🎧🎧');
+
 });
 
 app.listen(port, () => {
